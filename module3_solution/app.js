3296452f18c983377a8029d8d49487b2fca5eacb
Daniel <juma.daniel.otieno@gmail.com>
2016-10-03 13:04:53 +0300

    Correction

--- module3_solution/app.js
+++ module3_solution/app.js
@@ -2,60 +2,23 @@
   'use strict';
   angular.module('NarrowItDownApp',[])
   .controller('NarrowItDownController',NarrowItDownController)
-  .service('MenuSearchService',MenuSearchService)
-  .directive('foundItems',FoundItems);
-  function FoundItems(){
-    var ddo ={
-      templateUrl: 'foundItems.html',
-      scope: {
-        found: '<',
-        onRemove: '&'
-      },
-      controller:NarrowItDownController,
-      controllerAs:'list',
-      bindTocController: true
-    };
-    return ddo;
-  };
+  .service('MenuSearchService',MenuSearchService);
 
   NarrowItDownController.$inject = ['MenuSearchService'];
   function NarrowItDownController(MenuSearchService){
-    var list = this;
-    list.searchTerm ="";
-    list.message ="";
-    list.found =[];
-    list.narrowDown =function(){
-      list.found =[];
-      if(!list.searchTerm || list.searchTerm==""){
-        list.message ="Nothing found";
-      }else{
-        var promise = MenuSearchService.getMatchedMenuItems(list.searchTerm);
-        promise.then(function(result){
-          list.found=result;
-        });
-      }
-    };
-    list.removeItem =function(itemIndex){
-        list.found.splice(itemIndex,1);
+    var narrowItDownController = this;
+    narrowItDownController.found =ShoppingListCheckOffService.getMatchedMenuItems(narrowItDownController.searchTerm);
     };
   };
-  MenuSearchService.$inject=['$http'];
-  function MenuSearchService($http){
+  function MenuSearchService(){
     var service = this;
     service.getMatchedMenuItems = function(searchTerm){
       return $http({
-       method: 'GET',
-       url: ("https://davids-restaurant.herokuapp.com/menu_items.json")
+        method: "GET",
+        url: ("https://davids-restaurant.herokuapp.com/menu_items.json")
       }).then(function(result){
-        var foundItems =result.data;
-        for (var i=0;i<foundItems.length;i++){
-          if(foundItems[i].indexOf(searchTerm)==-1){
-            foundItems.splice(i,1);
-          };
-        };
-        return foundItems;
+        var founditems =result.data;
       });
     };
   };
-
 })();
