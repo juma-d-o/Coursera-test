c7b2eb362f230be458a75b7c47cb66a4822227b0
Daniel <juma.daniel.otieno@gmail.com>
2016-10-03 15:32:34 +0300

    Correction

--- module3_solution/app.js
+++ module3_solution/app.js
@@ -43,35 +43,24 @@
 
   };
 
-  MenuSearchService.$inject = ['$q', '$http'];
-  function MenuSearchService ($q, $http) {
-      var service = this;
-      service.getMatchedMenuItems = function(searchTerm) {
-          var deferred = $q.defer();
-          if (searchTerm.length == 0) {
-              deferred.resolve([]);
-              return deferred.promise;
-          }
-          return $http({
-              method : "GET",
-              url : "https://davids-restaurant.herokuapp.com/menu_items.json"
-          }).then(function (result) {
-              var foundItems = [];
-              var menu_items = result.data.menu_items;
-              console.log(searchTerm);
-              for (var i = 0; i < menu_items.length; i = i + 1) {
-                  if (menu_items[i].description.toUpperCase()
-                  .includes(searchTerm.toUpperCase()))
-                      foundItems.push(menu_items[i]);
-              }
+    MenuSearchService.$inject = ['$http'];
+  function MenuSearchService($http){
+    var service = this;
+    service.getMatchedMenuItems= function(searchTerm){
+    return $http({
+        method: "GET",
+        url: ("https://davids-restaurant.herokuapp.com/menu_items.json")
+      }).then(function(result){
+        var foundItems =result.data.menu_items;
+        for (var i = 0; i < foundItems.length; i++) {
+          var description=foundItems[i].description;
+          if(description.toLowerCase().indexOf(searchTerm)==-1){
+              foundItems.splice(i,1);
+          };
+        };
+        return foundItems;
+      });
+    };
 
-              deferred.resolve(foundItems);
-              return deferred.promise;
-          }).catch(function (result) {
-              console.log("http results : ", result );
-              deferred.reject("Error with http : ", result);
-              return deferred.promise;
-          });
-      }
-  }
+  };
 })();
