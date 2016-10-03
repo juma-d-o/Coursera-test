b52595541f5bff987e11dc62bf0e0d2aaa43168a
Daniel <juma.daniel.otieno@gmail.com>
2016-10-03 10:34:35 +0300

    Done with assignment

--- module3_solution/app.js
+++ module3_solution/app.js
@@ -8,7 +8,7 @@
     var ddo ={
       templateUrl: 'foundItems.html',
       scope: {
-        foundItems: '=',
+        found: '<',
         onRemove: '&'
       },
       controller:NarrowItDownController,
