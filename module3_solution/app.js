7036b0973f2e586bc1fbc039f7e0a982a46870c7
Daniel <juma.daniel.otieno@gmail.com>
2016-10-03 13:28:40 +0300

    Correction

--- module3_solution/app.js
+++ module3_solution/app.js
@@ -3,30 +3,26 @@
   angular.module('NarrowItDownApp',[])
   .controller('NarrowItDownController',NarrowItDownController)
   .service('MenuSearchService',MenuSearchService);
-
   NarrowItDownController.$inject = ['MenuSearchService'];
   function NarrowItDownController(MenuSearchService){
     var list = this;
-    list.narrowDown=function(){
-      var promise =MenuSearchService.getMatchedMenuItems(list.searchTerm);
-      promise.then(
-        function(result) {
-            narrowItDownController.found =result;
-        }
-      );
+     list.narrowDown=function() {
+       list.found = MenuSearchService.getMatchedMenuItems(this.searchTerm);
+     };
     };
-    };
-  };
+
     MenuSearchService.$inject = ['$http'];
   function MenuSearchService($http){
     var service = this;
-    service.getMatchedMenuItems = function(searchTerm){
-      return $http({
+    service.getMatchedMenuItems= function(searchTerm){
+      $http({
         method: "GET",
         url: ("https://davids-restaurant.herokuapp.com/menu_items.json")
       }).then(function(result){
-        var founditems =result.data;
+        var foundItems =result.data;
+        return foundItems;
       });
     };
+
   };
 })();
