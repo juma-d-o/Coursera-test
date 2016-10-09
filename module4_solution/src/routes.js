7645ca172ec5cfe91d8a21a9269f00a35ebddcaa
Daniel <juma.daniel.otieno@gmail.com>
2016-10-09 23:19:08 +0300

    Module 4 assignment

---
+++ module4_solution/src/routes.js
@@ -0,0 +1,27 @@
+(function(){
+  angular.module('MenuApp')
+  .config(RoutesConfig)
+  RoutesConfig.$inject=['$stateProvider','$urlRouterProvider'];
+  function RoutesConfig($stateProvider,$urlRouterProvider){
+    $urlRouterProvider.otherwise('/home');
+    $stateProvider
+    .state('home',{
+      url: '/home',
+      templateUrl:'src/home.html'
+    })
+
+   .state('categories',{
+     url: '/categories'
+     templateUrl: 'src/categories.html'
+   })
+
+   .state('items',{
+     url:'/items',
+     templateUrl: 'src/items.html'
+   })
+
+  }
+
+
+
+})()
