94f73c45a27415648ad25a1d170ad1288f730950
Daniel <juma.daniel.otieno@gmail.com>
2016-10-10 09:10:10 +0300

    Implemented categories component

---
+++ module4_solution/src/categories.component.js
@@ -0,0 +1,20 @@
+(function(){
+  'use strict';
+  angular.module('MenuApp')
+  .component('categories',{
+    templateUrl: 'src/categories.html',
+    controller:CategoriesComponentController,
+    resolve : {
+      categories : ['MenuDataService',function(MenuDataService){
+        return MenuDataService.getAllCategories();
+      }]
+    }
+  });
+
+  CategoriesComponentController.$inject=['categories'];
+  function CategoriesComponentController(categories){
+    var $ctrl =this;
+    $ctrl.categories =categories;
+  }
+
+})()
