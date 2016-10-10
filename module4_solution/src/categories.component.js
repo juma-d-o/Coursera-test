8660bb9404a55850c6d38bee95b30e1adb9e077e
Daniel <juma.daniel.otieno@gmail.com>
2016-10-10 11:51:05 +0300

    Implemented categories component

--- module4_solution/src/categories.component.js
+++ module4_solution/src/categories.component.js
@@ -2,10 +2,7 @@
   'use strict';
   angular.module('MenuApp')
   .component('categories',{
-    templateUrl: 'src/categories.template.html',
-    bindings : {
-      items : '<'
-    }
+    templateUrl: 'src/categories.template.html'
   });
 
 })()
