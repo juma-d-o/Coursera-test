69e7f2c828beee3d01b4a3492e6f70226d2e8a32
Daniel <juma.daniel.otieno@gmail.com>
2016-10-10 13:46:12 +0300

    Implemented categories component

--- module4_solution/src/routes.js
+++ module4_solution/src/routes.js
@@ -17,7 +17,9 @@
      controller: 'CategoriesController as categoryList',
      resolve : {
        items : ['MenuDataService',function(MenuDataService){
-          MenuDataService.getAllCategories()
+          MenuDataService.getAllCategories().then(function(results){
+            return results;
+          });
        }]
      }
    })
